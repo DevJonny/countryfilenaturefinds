a1e8c95f0092565b5ba6c5824ec3029d0204e6e9
Jonny Olliff-Lee <jonny.ollifflee@gmail.com>
2021-03-07 20:12:37 +0000

    Updates build with publish and copydocs

--- src/CountryFileNatureFinds.Build/Build.cs
+++ src/CountryFileNatureFinds.Build/Build.cs
@@ -1,18 +1,13 @@
-using System;
-using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [CheckBuildProjectConfigurations]
@@ -32,11 +27,13 @@ class Build : NukeBuild
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
-    [GitVersion] readonly GitVersion GitVersion;
+    [GitVersion(Framework = "netcoreapp3.1")] readonly GitVersion GitVersion;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath TestsDirectory => RootDirectory / "tests";
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    AbsolutePath Wwwwroot => ArtifactsDirectory / "wwwroot";
+    AbsolutePath Docs => RootDirectory / "docs";
 
     Target Clean => _ => _
         .Before(Restore)
@@ -67,4 +64,22 @@ class Build : NukeBuild
                 .EnableNoRestore());
         });
 
+    Target Publish => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetPublish(s => s
+                .SetProject(Solution)
+                .SetOutput(ArtifactsDirectory)
+                .EnableNoRestore()
+                .EnableNoBuild()
+            );
+        });
+
+    Target CopyDocs => _ => _
+        .DependsOn(Publish)
+        .Executes(() =>
+        {
+            CopyDirectoryRecursively(Wwwwroot, Docs, DirectoryExistsPolicy.Merge);
+        });
 }
