f1acd77b211d7da98ccc750d9bbabe7e9d71d4f6
Jonny Olliff-Lee <jonny.ollifflee@gmail.com>
2021-03-16 19:40:17 +0000

    Adds Family Photos sections

---
+++ src/CountryFileNatureFinds.Web/ViewModels/FamilyPhotoSection.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace CountryFileNatureFinds.Web.ViewModels
+{
+    public class FamilyPhotoSection
+    {
+        public string Name { get; set; }
+        public List<Photo> Photos { get; set; }
+    }
+}
---
+++ src/CountryFileNatureFinds.Web/ViewModels/Photo.cs
@@ -0,0 +1,9 @@
+namespace CountryFileNatureFinds.Web.ViewModels
+{
+    public class Photo
+    {
+        public string Link { get; set; }
+        public string Photographer { get; set; }
+        public string FormattedLink() => $"data/spring2021-showcase/{Link}";
+    }
+}
