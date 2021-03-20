569380b4bd563528377dff645106ce822dbab571
Jonny Olliff-Lee <jonny.ollifflee@gmail.com>
2021-03-20 19:06:46 +0000

    Resized Pa's Narcissi

--- src/CountryFileNatureFinds.Web/ViewModels/Photo.cs
+++ src/CountryFileNatureFinds.Web/ViewModels/Photo.cs
@@ -1,9 +1,11 @@
+using System;
+
 namespace CountryFileNatureFinds.Web.ViewModels
 {
     public class Photo
     {
         public string Link { get; set; }
         public string Photographer { get; set; }
-        public string FormattedLink() => $"data/spring2021-showcase/{Link}";
+        public string FormattedLink() => $"data/spring2021-showcase/{Link}?cb={Guid.NewGuid()}";
     }
 }
